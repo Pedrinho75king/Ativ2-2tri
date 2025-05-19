116b533079606644a4a4e37fcdd039d890336f14
Pedrinho75king <pedroyan@colegiopaulodetarso.com.br>
2025-05-19 16:10:51 +0000

    new.alterations

---
+++ alert2.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Botão com Alert 2</title>
+</head>
+<body>
+  <button onclick="mostrarMensagem()">Clique aqui</button>
+
+  <script src="script.js"></script>
+</body>
+</html>
